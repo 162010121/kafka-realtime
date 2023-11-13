ec029c86889f0f9866c62da779d0fa06e3e562bf
Praveen <Praveensonu1999@gmail.com>
2023-11-13 17:57:15 +0530

    Kafka-Realtime

---
+++ email-services/src/test/java/net/javaguides/Emailservice/EmailServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.Emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
